7b179d2122b6bdff85e687063d52d85ca7d407f9
Shruti Deshmukh <147811880+shruti15deshmukh@users.noreply.github.com>
2024-12-25 11:45:14 +0530

    1137 25/12

--- src/main/java/com/example/chat/repository/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.chat.repository;
-
-import com.example.chat.model.Group;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface GroupRepository extends MongoRepository<Group, String> {
-}
